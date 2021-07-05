cfabe79ffcf58bb1700dfccfe94cea2019124cce
Vladzo <vashchukvlad17@gmail.com>
2021-07-05 16:17:24 +0300

    old mongoose

--- constants/constants.js
+++ constants/constants.js
@@ -1,8 +1,8 @@
 module.exports = {
-  PORT: 3000,
+  PORT: process.env.PORT || 5000,
   UNKNOWN_ERROR: 'Unknown error',
   ROUTE_NOT_FOUND: 'Rout not fond',
-  DB: 'mongodb://localhost:27017/app-db',
+  DB: process.env.DB_CONNECTION_URL || 'mongodb://localhost:27017/app',
   SALT: 10,
   DELETE_ANSWER: 'User has been deleted!',
   UPDATE_ANSWER: 'User has been updated!'
