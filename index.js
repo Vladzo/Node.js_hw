e4e9c743bacfa02b2c641806283cc5391ada8a6c
Vlad Vashchuk <vashchukvlad17@gmail.com>
2021-09-08 11:19:10 +0300

    test branch

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+console.log('TEST');
 const { userRouter, authRouter } = require('./routes');
 const { constants, responseCodesEnum } = require('./constants');
 
